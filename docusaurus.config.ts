565565108eb227dc2a6dc73a9a7749c3a1687889
rowanna <rowankimna@gmail.com>
2025-01-21 23:51:26 +0900

    fix: edit meta tag img path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/IMG_8280.JPG",
     navbar: {
       title: `${ENV_VARIABLE.blogName}`,
       // logo: {
