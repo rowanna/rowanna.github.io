aa8ca973bcdeadc3802e826622d1fc81fdc88655
rowanna <rowankimna@gmail.com>
2025-01-24 14:37:37 +0900

    fix: backup

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+import { useRef, type ReactNode } from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+
+export default function Home(): ReactNode {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Rowanna's Tech Blog"
+    >
+      <main>About Page 개발중</main>
+    </Layout>
+  );
+}
