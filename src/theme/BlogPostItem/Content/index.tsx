2e9f27f885df9909ec0e7f8059c7caeba7c883c9
rowanna <rowankimna@gmail.com>
2025-02-04 16:15:18 +0900

    fix: node환경에서 window undefined인 현상 수정 최종 완료

--- src/theme/BlogPostItem/Content/index.tsx
+++ src/theme/BlogPostItem/Content/index.tsx
@@ -4,6 +4,46 @@ import { blogPostContainerID } from "@docusaurus/utils-common";
 import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 import MDXContent from "@theme/MDXContent";
 import type { Props } from "@theme/BlogPostItem/Content";
+import { useLocation } from "@docusaurus/router";
+import { useEffect } from "react";
+
+function HitsComponent() {
+  const location = useLocation(); // 현재 블로그 글의 URL 가져오기
+  const encodedUrl = encodeURIComponent(
+    `https://rowanna.github.io${location.pathname}`
+  );
+
+  return (
+    <div>
+      {/* Hits Counter 추가 */}
+      <a href="https://hits.seeyoufarm.com">
+        <img
+          src={`https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=${encodedUrl}&count_bg=%2379C83D&title_bg=%23555555&icon=blog&icon_color=%23E7E7E7&title=hits&edge_flat=false`}
+          alt="Hits"
+        />
+      </a>
+    </div>
+  );
+}
+const DisqusComments = () => {
+  const shortname = "rowanna";
+  const url = typeof window !== "undefined" ? window.location.href : "";
+  const location = useLocation(); // 현재 블로그 글의 URL 가져오기
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    if (window.DISQUS) {
+      window.DISQUS.reset({ reload: true });
+    } else {
+      const script = document.createElement("script");
+      script.src = `https://${shortname}.disqus.com/embed.js`;
+      script.setAttribute("data-timestamp", +new Date());
+      document.body.appendChild(script);
+    }
+  }, [shortname, url, location.pathname]);
+
+  return <div id="disqus_thread"></div>;
+};
 
 export default function BlogPostItemContent({
   children,
@@ -16,7 +56,10 @@ export default function BlogPostItemContent({
       id={isBlogPostPage ? blogPostContainerID : undefined}
       className={clsx("markdown", className)}
     >
+      {isBlogPostPage ? <HitsComponent /> : <></>}
       <MDXContent>{children}</MDXContent>
+
+      {isBlogPostPage ? <DisqusComments /> : <></>}
     </div>
   );
 }
--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -6,47 +6,6 @@ import BlogPostItemHeader from "@theme/BlogPostItem/Header";
 import BlogPostItemContent from "@theme/BlogPostItem/Content";
 import BlogPostItemFooter from "@theme/BlogPostItem/Footer";
 import type { Props } from "@theme/BlogPostItem";
-import { useEffect } from "react";
-import { useLocation } from "@docusaurus/router";
-
-function HitsComponent() {
-  const location = useLocation(); // 현재 블로그 글의 URL 가져오기
-  const encodedUrl = encodeURIComponent(
-    `https://rowanna.github.io${location.pathname}`
-  );
-
-  return (
-    <div>
-      {/* Hits Counter 추가 */}
-      <a href="https://hits.seeyoufarm.com">
-        <img
-          src={`https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=${encodedUrl}&count_bg=%2379C83D&title_bg=%23555555&icon=blog&icon_color=%23E7E7E7&title=hits&edge_flat=false`}
-          alt="Hits"
-        />
-      </a>
-    </div>
-  );
-}
-
-const DisqusComments = () => {
-  const shortname = "rowanna";
-  const url = typeof window !== "undefined" ? window.location.href : "";
-  const location = useLocation(); // 현재 블로그 글의 URL 가져오기
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    if (window.DISQUS) {
-      window.DISQUS.reset({ reload: true });
-    } else {
-      const script = document.createElement("script");
-      script.src = `https://${shortname}.disqus.com/embed.js`;
-      script.setAttribute("data-timestamp", +new Date());
-      document.body.appendChild(script);
-    }
-  }, [shortname, url, location.pathname]);
-
-  return <div id="disqus_thread"></div>;
-};
 
 // apply a bottom margin in list view
 function useContainerClassName() {
@@ -62,9 +21,7 @@ export default function BlogPostItem({
   return (
     <BlogPostItemContainer className={clsx(containerClassName, className)}>
       <BlogPostItemHeader />
-      <HitsComponent />
       <BlogPostItemContent>{children}</BlogPostItemContent>
-      <DisqusComments />
       <BlogPostItemFooter />
     </BlogPostItemContainer>
   );
