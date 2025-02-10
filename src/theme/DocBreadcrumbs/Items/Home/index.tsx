90462063071570ab0c30e2cdbe6729402af91026
rowanna <rowankimna@gmail.com>
2025-02-10 22:13:10 +0900

    feat: docs breadcrumbs home 경로 동적으로 설정

---
+++ src/theme/DocBreadcrumbs/Items/Home/index.tsx
@@ -0,0 +1,31 @@
+import React, { type ReactNode } from "react";
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { translate } from "@docusaurus/Translate";
+import IconHome from "@theme/Icon/Home";
+import React from "react";
+import { useLocation } from "@docusaurus/router";
+
+import styles from "./styles.module.css";
+
+export default function HomeBreadcrumbItem(): ReactNode {
+  const location = useLocation();
+  const basePath = `/${location.pathname.split("/")[1]}`; // 첫 번째 경로 추출
+  const homeHref = useBaseUrl(basePath);
+
+  return (
+    <li className="breadcrumbs__item">
+      <Link
+        aria-label={translate({
+          id: "theme.docs.breadcrumbs.home",
+          message: "Home page",
+          description: "The ARIA label for the home page in the breadcrumbs",
+        })}
+        className="breadcrumbs__link"
+        href={homeHref}
+      >
+        <IconHome className={styles.breadcrumbHomeIcon} />
+      </Link>
+    </li>
+  );
+}
