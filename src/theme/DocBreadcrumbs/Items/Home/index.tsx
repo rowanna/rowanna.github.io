16eb79caa10156577b9b604ea85f304c6f91e3c9
rowanna <rowankimna@gmail.com>
2025-02-10 22:22:53 +0900

    fix: breadcrumb 경로 수정

--- src/theme/DocBreadcrumbs/Items/Home/index.tsx
+++ src/theme/DocBreadcrumbs/Items/Home/index.tsx
@@ -10,7 +10,7 @@ import styles from "./styles.module.css";
 export default function HomeBreadcrumbItem(): ReactNode {
   const location = useLocation();
   const basePath = `/${location.pathname.split("/")[1]}`; // 첫 번째 경로 추출
-  const homeHref = useBaseUrl(basePath);
+  const homeHref = useBaseUrl(`${basePath}/intro`);
 
   return (
     <li className="breadcrumbs__item">
