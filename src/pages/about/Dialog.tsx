44cf5a45a59ec5c5b3f5d134ed10ea79e2a70b9f
rowanna <rowankimna@gmail.com>
2025-02-11 21:08:48 +0900

    fix: build 오류 수정

--- src/pages/about/Dialog.tsx
+++ src/pages/about/Dialog.tsx
@@ -34,14 +34,16 @@ const Dialog: React.FC<DialogProps> = ({ isOpen, dialogData, close }) => {
       backdrop.style.display = "none";
     }
 
-    // 블로그 포스트 로드 후 이미지에 줌 효과 추가
-    const zoom = mediumZoom(document.querySelectorAll("img"), {
-      margin: 24, // 이미지 확대 시 주변 여백
-    });
+    if (document) {
+      const zoom = mediumZoom(document.querySelectorAll("img"), {
+        margin: 24, // 이미지 확대 시 주변 여백
+      });
 
-    return () => {
-      zoom.detach(); // 컴포넌트 해제 시 줌 효과 제거
-    };
+      return () => {
+        zoom.detach(); // 컴포넌트 해제 시 줌 효과 제거
+      };
+    }
+    // 블로그 포스트 로드 후 이미지에 줌 효과 추가
   }, [isOpen, dialogData]);
   return (
     <>
