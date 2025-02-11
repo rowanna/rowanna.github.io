af6d49b931cbe07dd15559eda038b7d9487792c0
rowanna <rowankimna@gmail.com>
2025-02-11 12:17:52 +0900

    style: css 수정

--- src/theme/DocSidebar/Desktop/Content/index.tsx
+++ src/theme/DocSidebar/Desktop/Content/index.tsx
@@ -1,27 +1,27 @@
-import React, {type ReactNode, useState} from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
+import React, { type ReactNode, useState } from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
 import {
   useAnnouncementBar,
   useScrollPosition,
-} from '@docusaurus/theme-common/internal';
-import {translate} from '@docusaurus/Translate';
-import DocSidebarItems from '@theme/DocSidebarItems';
-import type {Props} from '@theme/DocSidebar/Desktop/Content';
+} from "@docusaurus/theme-common/internal";
+import { translate } from "@docusaurus/Translate";
+import DocSidebarItems from "@theme/DocSidebarItems";
+import type { Props } from "@theme/DocSidebar/Desktop/Content";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 function useShowAnnouncementBar() {
-  const {isActive} = useAnnouncementBar();
+  const { isActive } = useAnnouncementBar();
   const [showAnnouncementBar, setShowAnnouncementBar] = useState(isActive);
 
   useScrollPosition(
-    ({scrollY}) => {
+    ({ scrollY }) => {
       if (isActive) {
         setShowAnnouncementBar(scrollY === 0);
       }
     },
-    [isActive],
+    [isActive]
   );
   return isActive && showAnnouncementBar;
 }
@@ -36,17 +36,19 @@ export default function DocSidebarDesktopContent({
   return (
     <nav
       aria-label={translate({
-        id: 'theme.docs.sidebar.navAriaLabel',
-        message: 'Docs sidebar',
-        description: 'The ARIA label for the sidebar navigation',
+        id: "theme.docs.sidebar.navAriaLabel",
+        message: "Docs sidebar",
+        description: "The ARIA label for the sidebar navigation",
       })}
       className={clsx(
-        'menu thin-scrollbar',
+        "menu thin-scrollbar",
         styles.menu,
         showAnnouncementBar && styles.menuWithAnnouncementBar,
-        className,
-      )}>
-      <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, 'menu__list')}>
+        className
+      )}
+    >
+      <h4>👩‍💻 All Posts</h4>
+      <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, "menu__list")}>
         <DocSidebarItems items={sidebar} activePath={path} level={1} />
       </ul>
     </nav>
